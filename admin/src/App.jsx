d9c129f4fd06f42b7d2ac4a3f56d874f780bffca
Manoj <punmanoj2060@gmail.com>
2025-05-25 14:14:48 +0545

    hhhh

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -23,6 +23,7 @@ const App = () => {
         <Sidebar />
         <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
           <Routes>
+            <Route path="/" element={<CreatePost />} />
             <Route path="/create-post" element={<CreatePost />} />
             <Route path="/application" element={<Applications />} />
             <Route path="/blooddonation" element={<BloodDonation />} />
